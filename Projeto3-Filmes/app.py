7589d453b593d13c50a7da5e03eed8fa8f3c9f4f
David Sotto Mayor <ddsmmf@gmail.com>
2021-06-29 20:18:00 -0400

    Buscando todos os registros de filmes do banco de dados

--- Projeto3-Filmes/app.py
+++ Projeto3-Filmes/app.py
@@ -1,9 +1,35 @@
 from flask import (Flask, Blueprint, render_template)
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 bp = Blueprint('app', __name__)
 
-filmes = []
+
+# Conectando com o banco de dados
+user = 'zkcsyvoy'
+password = '9Mtctta5J_Rs98ORf1i8Mxl2PtVUwF2W'
+hostname = 'tuffi.db.elephantsql.com'
+database = 'zkcsyvoy'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{user}:' + \
+    f'{password}@{hostname}/{database}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Filmes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(255), nullable=False)
+    image_url = db.Column(db.String(255), nullable=False)
+
+    def __init__(self, nome, image_url):
+        self.nome = nome
+        self.image_url = image_url
+
+    @staticmethod
+    def read_all():
+        # Buscando todos os filmes do DB
+        return Filmes.query.all()
 
 
 @bp.route('/')
@@ -14,7 +40,9 @@ def home():
 @bp.route('/read')
 def listar_filmes():
     # Passando para dentro do nosso HTML os dados da minha listagem de filmes!
-    return render_template('listar-filmes.html', listaDeFilmes=filmes)
+    registros = Filmes.read_all()
+    print(registros)
+    return render_template('listar-filmes.html', registros=registros)
 
 
 @bp.route('/read/<id_filme>')
@@ -22,7 +50,7 @@ def lista_detalhe_filme(id_filme):
     return 'Página em Construção para o filme com ID -> ' + id_filme
 
 
-# Pega os dados do blueprint da nossa aplicação (nome do app e as rotas) e 
+# Pega os dados do blueprint da nossa aplicação (nome do app e as rotas) e
 # registra dentro do app do Flask
 app.register_blueprint(bp)
 
